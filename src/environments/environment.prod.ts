efe44521933631611753512f37c094bbc01a7f5f
RedanHassoun <redan.has.1991@gmail.com>
2019-01-15 23:18:36 +0200

    Configured firebase and added a basic starter bootstrap template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAsG8W8UjF4n8dPRRjRzCbCPBzNInQTQ1k",
+    authDomain: "oshop-f3d23.firebaseapp.com",
+    databaseURL: "https://oshop-f3d23.firebaseio.com",
+    projectId: "oshop-f3d23",
+    storageBucket: "oshop-f3d23.appspot.com",
+    messagingSenderId: "327814737622"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAsG8W8UjF4n8dPRRjRzCbCPBzNInQTQ1k",
+    authDomain: "oshop-f3d23.firebaseapp.com",
+    databaseURL: "https://oshop-f3d23.firebaseio.com",
+    projectId: "oshop-f3d23",
+    storageBucket: "oshop-f3d23.appspot.com",
+    messagingSenderId: "327814737622"
+  }
 };
 
 /*
