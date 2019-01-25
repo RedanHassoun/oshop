c0450d9fe66cd5811f13169a633cff84e2e40928
RedanHassoun <redan.has.1991@gmail.com>
2019-01-25 14:34:34 +0200

    implemented 'add-to-cart'

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, DatabaseSnapshot } from '@angular/fire/database';
 import { AppConsts } from '../common/appconsts';
 import { Product } from '../model/product';
+import { take } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,16 +20,44 @@ export class ShoppingCartService {
     return this.db.object(AppConsts.DB_SHOPPING_CARTS + '/' + cartId);
   }
 
-  private async getOrCreateCart(){
+  private async getOrCreateCartId(){
     let cartId = localStorage.getItem(AppConsts.KEY_CART_ID);
-    if(!cartId){
-      let result = await this.create();
-      localStorage.setItem(AppConsts.KEY_CART_ID,result.key);
-      return this.getCart(result.key); 
-    }
-    return this.getCart(cartId);
+    if(cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem(AppConsts.KEY_CART_ID,result.key);
+    return result.key;
   }
 
-  addToCart(product: Product) {
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    const cartItemPath = AppConsts.DB_SHOPPING_CARTS + 
+                        '/' + 
+                        cartId + 
+                        '/items'+ 
+                        product.$key;
+    let item$ = this.db.object(cartItemPath)
+                        .snapshotChanges();
+    item$.pipe(take(1))
+      .subscribe((item)=>{
+        if(item.payload.exists()){
+          const itemFromPayload = this.getItemFromPayload(item.payload);
+          this.db.object(cartItemPath).update({
+            quantity: itemFromPayload.quantity + 1
+          })
+        }else{
+          this.db.object(cartItemPath).set({
+            quantity: 1
+          })
+        }
+      })
+  }
+
+  private getItemFromPayload(payload:DatabaseSnapshot<{}>){
+    let obj = payload.val()
+    let item = {
+      quantity: obj['quantity']
+    }
+    return item;
   }
 }
