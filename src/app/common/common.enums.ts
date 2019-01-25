7629a637624d8bdecc9ff15feeea26564405f5fd
RedanHassoun <redan.has.1991@gmail.com>
2019-01-25 16:58:58 +0200

    Implemented the change quantity buttons

---
+++ src/app/common/common.enums.ts
@@ -0,0 +1,1 @@
+export enum Operation { ADD,REMOVE };
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -3,12 +3,50 @@ import { AngularFireDatabase, DatabaseSnapshot } from '@angular/fire/database';
 import { AppConsts } from '../common/appconsts';
 import { Product } from '../model/product';
 import { take } from 'rxjs/operators';
+import { Operation } from '../common/common.enums';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
+  // TODO: refactor the add to cart logic
   constructor(private db:AngularFireDatabase) { }
+
+  addToCart(product: Product) {
+    this.modifyCartQuantity(product,Operation.ADD)
+  }
+
+  removeFromCart(product: Product): any {
+    this.modifyCartQuantity(product,Operation.REMOVE)
+  }
+
+  private async modifyCartQuantity(product:Product,op:Operation){
+    let cartId = await this.getOrCreateCartId();
+    let toAdd = (op === Operation.ADD) ? 1 : -1;
+    const cartItemPath = AppConsts.DB_SHOPPING_CARTS + 
+                        '/' + 
+                        cartId + 
+                        '/items/'+ 
+                        product.$key;
+    let item$ = this.db.object(cartItemPath)
+                        .snapshotChanges();
+    item$.pipe(take(1))
+      .subscribe((item)=>{
+        if(item.payload.exists()){
+          const itemFromPayload = this.getItemFromPayload(item.payload);
+          if( (itemFromPayload.quantity + toAdd) < 0)
+            throw new Error("Quantity can't be negative");
+
+          this.db.object(cartItemPath).update({
+            quantity: itemFromPayload.quantity + toAdd
+          })
+        }else{
+          this.db.object(cartItemPath).set({
+            quantity: 1
+          })
+        }
+      })
+  }
 
   private create() {
     return this.db.list(AppConsts.DB_SHOPPING_CARTS).push({
@@ -28,30 +66,6 @@ export class ShoppingCartService {
     let result = await this.create();
     localStorage.setItem(AppConsts.KEY_CART_ID,result.key);
     return result.key;
-  }
-
-  async addToCart(product: Product) {
-    let cartId = await this.getOrCreateCartId();
-    const cartItemPath = AppConsts.DB_SHOPPING_CARTS + 
-                        '/' + 
-                        cartId + 
-                        '/items/'+ 
-                        product.$key;
-    let item$ = this.db.object(cartItemPath)
-                        .snapshotChanges();
-    item$.pipe(take(1))
-      .subscribe((item)=>{
-        if(item.payload.exists()){
-          const itemFromPayload = this.getItemFromPayload(item.payload);
-          this.db.object(cartItemPath).update({
-            quantity: itemFromPayload.quantity + 1
-          })
-        }else{
-          this.db.object(cartItemPath).set({
-            quantity: 1
-          })
-        }
-      })
   }
 
   private getItemFromPayload(payload:DatabaseSnapshot<{}>){
