83e999532dd0001efccb063367ef94e8a2ab825c
RedanHassoun <redan.has.1991@gmail.com>
2019-02-01 12:14:06 +0200

    Added usage of async pipe in the checkout component

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,27 +1,25 @@
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingCartService } from '../services/shopping-cart.service';
 import { ShoppingCart } from '../model/shopping-cart';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit,OnDestroy {
-  cart:ShoppingCart;
-  cartSubscription:Subscription; // TODO: use the async pipe
+export class CheckOutComponent implements OnInit {
+  cart$:Observable<ShoppingCart>; 
 
   constructor(private shoppingCartService:ShoppingCartService) {}
 
   async ngOnInit() {
     let cart = await this.shoppingCartService.getCart();
-    this.cartSubscription = cart.snapshotChanges().subscribe(res=>{
-      this.cart = this.shoppingCartService.angularFireActionToCartObject(res);
-    })
-  }
-
-  ngOnDestroy(): void {
-    this.cartSubscription.unsubscribe();
+    this.cart$ = cart.snapshotChanges()
+          .pipe(map(res=>{
+              return this.shoppingCartService
+                .angularFireActionToCartObject(res);
+         }))
   }
 }
