1c6725f03ff735fdc2cc47c85a77fc64a2692b64
RedanHassoun <redan.has.1991@gmail.com>
2019-02-04 22:49:55 +0200

    Added the shopping module

---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,42 @@
+import { SharedModule } from 'shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ProductsFilterComponent } from './components/products/products-filter/products-filter.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { FormsModule } from '@angular/forms';
+import { AuthGaurd } from 'shared/services/auth-gaurd.service';
+import { DataTableModule } from 'angular-6-datatable';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    DataTableModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductsComponent},
+      { path: 'shopping-cart', component: ShoppingCartComponent},
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGaurd]},
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGaurd]},
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGaurd]},
+    ])
+  ],
+  declarations: [
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    ProductsFilterComponent,
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent
+  ]
+})
+export class ShoppingModule { }
